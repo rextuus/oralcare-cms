bca9af5b7a6d23d30820651722145611592845ed
Wolfgang Hinzmann <wolfgang.hinzmann@doccheck.com>
2026-03-06 14:23:18 +0100

    WIP

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const Encore = require('@symfony/webpack-encore');
+
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+    .setOutputPath('public/build/website/')
+    .setPublicPath('/build/website')
+    .addStyleEntry('app', './assets/website/scss/style.scss')
+    .disableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+    .enableSassLoader()
+;
+
+module.exports = Encore.getWebpackConfig();
